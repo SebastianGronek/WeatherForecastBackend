1cdd13fc6053c057a6493aba83ce14424935d015
Sebastian Gronek <groneks.a@gmail.com>
2021-11-10 00:15:05 +0100

    initial commit

---
+++ src/test/java/sunshine/WeatherForecastBackend/WeatherForecastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sunshine.WeatherForecastBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
